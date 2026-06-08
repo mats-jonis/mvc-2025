1806020272e21bd510d6e9955a4e15017668163d
mats-jonis <majt24@student.bth.se>
2026-06-08 11:15:57 +0200

    Add documentation to Deck class

--- src/Card/Deck.php
+++ src/Card/Deck.php
@@ -2,27 +2,45 @@
 
 namespace App\Card;
 
+/**
+ * Represents a deck of playing cards.
+ *
+ * Cards can be added, shuffled and drawn from the top.
+ */
 class Deck
 {
-    /** @var Card[] */
+    /** @var Card[] The cards currently in the deck. */
     private array $deck = [];
 
+    /**
+    * Add a card to the deck.
+    */
     public function add(Card $card): void
     {
         $this->deck[] = $card;
     }
 
+    /**
+     * Shuffle the cards in the deck into a random order.
+     */
     public function shuffle(): void
     {
         shuffle($this->deck);
     }
 
+    /**
+     * Get the number of cards left in the deck.
+     */
     public function getNumberCards(): int
     {
         return count($this->deck);
     }
 
-    /** @return int[] */
+    /**
+     * Get the value of every card in the deck.
+     *
+     * @return int[] The numeric value of each card, in order.
+     */
     public function getValues(): array
     {
         $values = [];
@@ -32,7 +50,11 @@ class Deck
         return $values;
     }
 
-    /** @param int[] $values */
+    /**
+     * Set the value of each card by index.
+     *
+     * @param int[] $values The new values, applied to the cards in order.
+     */
     public function setValues(array $values): void
     {
         foreach ($values as $index => $value) {
@@ -40,7 +62,11 @@ class Deck
         }
     }
 
-    /** @return string[] */
+    /**
+     * Get the string representation of every card in the deck.
+     *
+     * @return string[] The string representation of each card, in order.
+     */
     public function getString(): array
     {
         $values = [];
@@ -50,6 +76,11 @@ class Deck
         return $values;
     }
 
+    /**
+     * Draw and remove the top card from the deck.
+     *
+     * @return Card|null The drawn card, or null if the deck is empty.
+     */
     public function draw(): ?Card
     {
         return array_pop($this->deck);
