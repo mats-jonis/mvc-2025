d81280b3969c1d85cffe307340155948deffaab8
mats-jonis <majt24@student.bth.se>
2026-06-27 19:04:42 +0200

    Add database page with ER diagram and ORM discussion

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -19,4 +19,10 @@ class ProjController extends AbstractController
     {
         return $this->render('proj/about.html.twig');
     }
+
+    #[Route("/proj/about/database", name: "proj_database")]
+    public function database(): Response
+    {
+        return $this->render('proj/database.html.twig');
+    }
 }
